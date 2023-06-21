a981dfbeb646802590a6b6771fbf92ee517f568f
Sampul-CodeMine <studysampul@gmail.com>
2023-06-21 13:06:17 +0100

    Solution to Task 3. Creating a DB and a User for Dev platform

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,11 @@
+-- Creates a Schema/database if not exist
+CREATE SCHEMA IF NOT EXISTS `hbnb_dev_db`;
+
+-- Drop user if already existing and create a new user
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the newly created user
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
