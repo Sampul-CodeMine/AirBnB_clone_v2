269f97fc4546fb313a43a1153b9404bd3037a1b7
Obianuju23 <obianuju23@gmail.com>
2023-06-21 15:47:11 -0700

    Solution to Task 4. Creating a DB and a User for Test platform

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ CREATE SCHEMA IF NOT EXISTS `hbnb_dev_db`;
 DROP USER IF EXISTS 'hbnb_dev'@'localhost';
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
--- Grant privileges to the newly created user
+-- Grant some privileges to the newly created user
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
