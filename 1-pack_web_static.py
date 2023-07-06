8c38b027c0a3ff5935caeadcea6981fcb8e7b04f
Dukeson Ehigboria <studysampul@gmail.com>
2023-07-06 18:38:59 +0100

    Solution to Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2
+"""
+Module that generates a .tgz archive from the web_static directory
+"""
+import os.path as osp
+from datetime import datetime as dt
+from fabric.pai import local
+
+def do_pack():
+    """
+    A function that creates a .tgz archive file containing all the
+    files in the web_static folder
+    """
+    date = dt.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour.
+                                                         date.minute,
+                                                         date.second)
+    if not osp.isdir("versions"):
+        if local("mkdir -p verisons").failed is True:
+            return (None)
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return (None)
+    return (file)
