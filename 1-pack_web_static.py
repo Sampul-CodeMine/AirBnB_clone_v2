26c1affd9cd565df437bfbba918c6d5fd3ac5023
Dukeson Ehigboria <studysampul@gmail.com>
2023-07-06 19:03:07 +0100

    Removed bugs from fabfile

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -19,8 +19,8 @@ def do_pack():
                                                          date.hour.
                                                          date.minute,
                                                          date.second)
-    if not osp.isdir("versions"):
-        if local("mkdir -p verisons").failed is True:
+    if osp.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
             return (None)
     if local("tar -cvzf {} web_static".format(file)).failed is True:
         return (None)
