d98a9951c30bcbf57f459eb69d85746a9d89c22c
Dukeson Ehigboria <studysampul@gmail.com>
2023-07-07 04:19:17 -0700

    Added the right libraries for fabric

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,7 +4,7 @@ Module that generates a .tgz archive from the web_static directory
 """
 import os.path as osp
 from datetime import datetime as dt
-from fabric import Connection
+from fabric.api import local
 
 
 def do_pack():
@@ -12,17 +12,16 @@ def do_pack():
     A function that creates a .tgz archive file containing all the
     files in the web_static folder
     """
-    con = Connection('217820-web-01')
     date = dt.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
                                                          date.month,
                                                          date.day,
-                                                         date.hour.
+                                                         date.hour,
                                                          date.minute,
                                                          date.second)
     if osp.isdir("versions") is False:
-        if con.local("mkdir -p versions").failed is True:
+        if local("mkdir -p versions").failed is True:
             return (None)
-    if con.local("tar -cvzf {} web_static".format(file)).failed is True:
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return (None)
     return (file)
