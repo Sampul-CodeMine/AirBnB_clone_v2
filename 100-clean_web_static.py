570cf850400370293e5c06b5c2d614e22ed1e379
Dukeson Ehigboria <studysampul@gmail.com>
2023-07-07 17:58:50 +0100

    Solution to task 100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module to delete out-of-date archives"""
+import os
+from fabric.api import *
+
+"""setting the environment host for the servers"""
+env.hosts = ['3.84.237.114', '100.25.131.115']
+
+
+def do_clean(number=0):
+    """
+    This is a method/function that removes outdated archives
+
+    Args:
+        number (int): The number of archives to keep
+    Description:
+        If number = 0 || 1, keep only the most recent archive
+        if number is 2, keep the 2 most recent archives
+    Returns:
+        Nothing
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    zipfiles = sorted(os.listdir("versions"))
+    [zipfiles.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for zipfile in zipfiles]
+
+    with cd("/data/web_static/releases"):
+        zipfiles = run("ls -tr").split()
+        zipfiles = [a for zipfile in zipfiles if "web_static_" in zipfile]
+        [zipfiles.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for zipfile in zipfiles]
